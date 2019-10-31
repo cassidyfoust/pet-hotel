be316023a2b02e9c0c7f64b2483cc38d9a4f8a7f
Cassidy Foust <cassidy.foust@gmail.com>
2019-10-31 09:52:43 -0500

    pet history setup on dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddPet from './components/AddPet/AddPet'
 
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
+      Pet Hotel
       </header>
+      <body>
+        <div className="toggle-view">
+          <button className="toggleBtn">Dashboard</button>
+          <button className="toggleBtn">Manage Owners</button>
+        </div>
+        <AddPet/>
+        <PetHistory/>
+      </body>
     </div>
   );
 }
---
+++ src/components/AddPet/AddPet.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// const mapStateToProps = reduxState => ({
+//     reduxState,
+// });
+
+class AddPet extends Component {
+    state = {}
+    render() {
+        return (
+            <>
+            <div className="header">
+                <h2>Add Pet</h2>
+                </div>
+            <div className="add-pet-form">
+                <input placeholder="Pet Name" className="pet-form"></input>
+                <input placeholder="Pet Color" className="pet-form"></input>
+                <input placeholder="Pet Breed" className="pet-form"></input>
+                <select className="pet-form">
+                    <option>Test 1</option>
+                    <option>Test 2</option>
+                    <option>Test 3</option>
+                </select>
+                <button>Submit</button>
+            </div>
+            </>
+        )
+    }
+}
+export default AddPet;
